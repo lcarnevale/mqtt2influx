99fbb45704116fc0bce70c3905191cbb10bdf20d
Lorenzo Carnevale <lorenzocarnevale@gmail.com>
2021-02-01 10:10:28 +0100

    created a dedicate writer class

--- app/proxy.py
+++ app/proxy.py
@@ -13,108 +13,11 @@ __author__ = 'Lorenzo Carnevale <lcarnevale@unime.it>'
 __credits__ = ''
 __description__ = 'MQTT to InfluxDB Proxy'
 
-import json
+
 import yaml
-import socket
-import logging
 import argparse
-import threading
-import persistqueue
-import paho.mqtt.client as mqtt
+from writer import Writer
 from reader import Reader
-
-
-def writer_job(host, port, topics):
-    q = persistqueue.SQLiteQueue('data', auto_commit=True)
-
-    # initializing metadata
-    client_name = '%s-sub' % (socket.gethostname())
-
-    # implementing callbacks
-    def on_connect(client, _, __, rc):
-        """Connection's callback
-
-        The callback for when the client receives a CONNACK response from the server.
-        It subscribes to all the topics specified in the topics list.
-        Moreover, subscribing in on_connect() means that if we lose the connection
-        and reconnect then subscriptions will be renewed.
-
-        Connection Return Codes:
-            0: Connection successful
-            1: Connection refused – incorrect protocol version
-            2: Connection refused – invalid client identifier
-            3: Connection refused – server unavailable
-            4: Connection refused – bad username or password
-            5: Connection refused – not authorised
-            6-255: Currently unused.
-
-        Args:
-            client(obj:'paho.mqtt.client.Client'): the client instance for this callback;
-            rc(int): is used for checking that the connection was established.
-        """
-        return_code = {
-            0: "Connection successful",
-            1: "Connection refused – incorrect protocol version",
-            2: "Connection refused – invalid client identifier",
-            3: "Connection refused – server unavailable",
-            4: "Connection refused – bad username or password",
-            5: "Connection refused – not authorised",
-        }
-        print(return_code.get(rc, "Currently unused."))
-        try:
-            for topic in topics:
-                client.subscribe(topic)
-                print("Subscribed to %s" % (topic))
-        except Exception as e:
-            print(e)
-
-    def on_disconnect(client, _, rc):
-        """
-        Args:
-            client(obj:'paho.mqtt.client.Client'): the client instance for this callback;
-            rc(int): is used for checking that the disconnection was done.
-        """
-        print('Disconnection successful %s' % (rc))
-        del q
-
-    def on_message(client, _, msg):
-        """Message's callback
-
-        The callback for when a PUBLISH message is received from the server.
-
-        Args:
-            client(obj:'paho.mqtt.client.Client'): the client instance for this callback;
-            msg(): an instance of MQTTMessage.
-        """
-        try:
-            payload = json.loads(msg.payload)
-            q.put(payload)
-            print('Just written %s - %s' % (msg.topic, payload))
-        except ValueError:
-            print('Message is malformed. JSON required.')
-        except Exception as e:
-            print(e)
-
-    # defining the client and callbacks
-    client = mqtt.Client(client_name)
-    client.on_connect = on_connect
-    client.on_disconnect = on_disconnect
-    client.on_message = on_message
-
-    # authenticating and connecting to the broker
-    # client.username_pw_set(username=options.username,password=options.password)
-    client.connect(host, port)
-
-    # Blocking call that processes network traffic, dispatches callbacks and
-    # handles reconnecting.
-    # Other loop*() functions are available that give a threaded interface and a
-    # manual interface.
-    try:
-        client.loop_forever()
-    except KeyboardInterrupt:
-        pass
-    finally:
-        client.disconnect()
 
 
 def main():
@@ -133,9 +36,6 @@ def main():
 
     options = parser.parse_args()
 
-    format = "%(asctime)s: %(message)s"
-    logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
-
     with open(options.config) as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
 
@@ -145,10 +45,9 @@ def main():
     reader.start()
 
 def setup_writer(config):
-    return threading.Thread(
-        target = writer_job, 
-        args = (config['host'],config['port'], config['topics'])
-    )
+    writer = Writer(config['host'], config['port'], config['topics'])
+    writer.setup()
+    return writer
 
 def setup_reader(config):
     reader = Reader(config['host'], config['port'], config['token'],
---
+++ app/writer.py
@@ -0,0 +1,130 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+
+"""Reader class based on MQTT
+
+This implementation does its best to follow the Robert Martin's Clean code guidelines.
+The comments follows the Google Python Style Guide:
+    https://github.com/google/styleguide/blob/gh-pages/pyguide.md
+"""
+
+__copyright__ = 'Copyright 2021, FCRlab at University of Messina'
+__author__ = 'Lorenzo Carnevale <lcarnevale@unime.it>'
+__credits__ = ''
+__description__ = 'Reader class based on MQTT'
+
+
+import json
+import socket
+import logging
+import threading
+import persistqueue
+import paho.mqtt.client as mqtt
+
+
+class Writer:
+
+    def __init__(self, host, port, topics):
+        self.__host = host
+        self.__port = port
+        self.__topics = topics
+        self.__client_name = '%s-sub' % (socket.gethostname())
+        self.__writer = None
+        self.__setup_logging()
+
+    def __setup_logging(self):
+        format = "%(asctime)s %(filename)s:%(lineno)d %(levelname)s - %(message)s"
+        datefmt = "%d/%m/%Y %H:%M:%S"
+        logging.basicConfig(format=format, level=logging.DEBUG, datefmt=datefmt)
+
+
+    def setup(self):
+        self.__writer = threading.Thread(
+            target = self.__writer_job, 
+            args = (self.__host, self.__port, self.__topics)
+        )
+
+    def __writer_job(self, host, port, topics):
+        # SQLite objects created in a thread can only be used in that same thread.
+        self.__queue = persistqueue.SQLiteQueue('data', auto_commit=True)
+
+        # defining the client and callbacks
+        client = mqtt.Client(self.__client_name)
+        client.on_connect = self.__on_connect
+        client.on_disconnect = self.__on_disconnect
+        client.on_message = self.__on_message
+
+        # client.username_pw_set(username=options.username,password=options.password)
+        client.connect(host, port)
+
+        try:
+            client.loop_forever()
+        except KeyboardInterrupt:
+            pass
+        finally:
+            client.disconnect()
+
+    """Connection's callback
+
+    The callback used when the client receives a CONNACK response from the server.
+    Subscribing to on_connect() means that the connection is renewed when it is lost.
+
+    Args:
+        client (obj:'paho.mqtt.client.Client'): the client instance for this callback
+        rc (int): is used for checking that the connection was established
+    """
+    def __on_connect(self, client, _, __, rc):
+        return_code = {
+            0: "Connection successful",
+            1: "Connection refused – incorrect protocol version",
+            2: "Connection refused – invalid client identifier",
+            3: "Connection refused – server unavailable",
+            4: "Connection refused – bad username or password",
+            5: "Connection refused – not authorised",
+        }
+        if (rc == 0):
+            try:
+                for topic in self.__topics:
+                    logging.debug("Subscription to %s ..." % (topic))
+                    result, mid = client.subscribe(topic)
+                    if (result != mqtt.MQTT_ERR_SUCCESS):
+                        raise RuntimeError(result)
+                    logging.info("Subscription to %s ... completed" % (topic))
+            except Exception as e:
+                logging.error(e)
+        else:
+            logging.error(return_code[rc])
+
+    """MQTT Disconnection callback
+
+    Args:
+        client (obj:'paho.mqtt.client.Client'): the client instance for this callback
+        rc (int): is used for checking that the disconnection was done
+    """
+    def __on_disconnect(self, client, _, rc):
+        logging.info('Disconnection successful %s' % (rc))
+        del self.__queue
+
+    """MQTT message received callback
+
+    The callback used when a published message is received from the server.
+
+    Args:
+        client (obj:'paho.mqtt.client.Client'): the client instance for this callback
+        msg (): an instance of MQTTMessage
+    """
+    def __on_message(self, client, _, msg):
+        try:
+            logging.info(msg.payload)
+            payload = json.loads(msg.payload)
+            logging.debug('Received JSON data from %s' % (msg.topic))
+            self.__queue.put(payload)
+            logging.debug('JSON data insert into the queue')
+        except ValueError:
+            logging.error('Message received from %s is malformed. JSON required.' % (msg.topic))
+        except Exception as e:
+            logging.error(e)
+
+
+    def start(self):
+        self.__writer.start()
